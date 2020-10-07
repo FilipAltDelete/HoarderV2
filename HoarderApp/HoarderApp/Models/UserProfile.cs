ca8aa93dd85595335b4ec581c5b792c27679e27e
Petar Björkman <bagar@Petars-MacBook-Pro.local>
2020-10-07 16:14:14 +0200

    fixed Browse users

--- HoarderApp/HoarderApp/Models/UserProfile.cs
+++ HoarderApp/HoarderApp/Models/UserProfile.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using Newtonsoft.Json;
 namespace HoarderApp.Models
 {
     public class UserProfile
     {
-
+        [JsonProperty("id")]
         public int Id { get; set; }
+        [JsonProperty("accountDetailsId")]
         public int AccountDetailsId { get; set; }
+        [JsonProperty("location")]
         public string Location { get; set; }
+        [JsonProperty("contact")]
         public string Contact { get; set; }
+        [JsonProperty("profileName")]
         public string ProfileName { get; set; }
 
         
---
+++ HoarderApp/HoarderApp/Views/UserProfilePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using HoarderApp.Models;
+using Xamarin.Forms;
+
+namespace HoarderApp.Views
+{
+    public partial class UserProfilePage : ContentPage
+    {
+        AccountDetails SignedInUser;
+        UserProfile SelectedUser;
+        public UserProfilePage(AccountDetails signedInUser,UserProfile selectedUser)
+        {
+            SignedInUser = signedInUser;
+            SelectedUser = selectedUser;
+            InitializeComponent();
+        }
+
+    }
+}
